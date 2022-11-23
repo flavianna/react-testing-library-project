3a53107d22e130eaa6b7f28b2fa2fdd2d67867b6
flavianna <flaviannabe@gmail.com>
2022-11-22 21:24:14 -0300

    Requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Renderiza a pokedex e...', () => {
+  describe('Testa o Home e...', () => {
+    test('verifica se é exibido na tela um link com o texto Home', () => {
+      renderWithRouter(<App />);
+      const home = screen.getByRole('link', {
+        name: /home/i,
+      });
+      expect(home).toBeInTheDocument();
+    });
+  });
+
+  describe('Testa o about e...', () => {
+    test('verifica se é exibido na tela um link com o texto about', () => {
+      renderWithRouter(<App />);
+      const about = screen.getByRole('link', {
+        name: /about/i,
+      });
+      expect(about).toBeInTheDocument();
+    });
+  });
+
+  describe('Testa o favorite Pokemon e...', () => {
+    test('verifica se é exibido na tela um link com o texto favorite Pokemon', () => {
+      renderWithRouter(<App />);
+      const favPokemon = screen.getByRole('link', {
+        name: /favorite pokémon/i,
+      });
+      expect(favPokemon).toBeInTheDocument();
+    });
+  });
+});
