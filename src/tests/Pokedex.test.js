5b890647a98503b0cb4379b06d5424c388836456
flavianna <flaviannabe@gmail.com>
2022-11-24 23:25:39 -0300

    Requisito 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente Pokédex e...', () => {
+  test('verifica se a página contém um heading h2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />);
+    const verifyTag = screen.getByRole('heading', {
+      name: /encountered pokémon/i,
+    });
+    expect(verifyTag).toBeInTheDocument();
+  });
+
+  test('verifica se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
+    renderWithRouter(<App />);
+    const buttonText = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    userEvent.click(buttonText);
+    const Pokemon = screen.getByText('Charmander');
+    expect(Pokemon).toBeInTheDocument();
+  });
+
+  test('verifica se é mostrado apenas um Pokémon por vez;', () => {
+    renderWithRouter(<App />);
+    const PokemonName = screen.getAllByTestId('pokemon-name');
+    // getAllBy retorna um array
+    expect(PokemonName[0]).toBeInTheDocument();
+  });
+
+  test('verifica se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+
+    const buttonTypeFilter = screen.getAllByTestId('pokemon-type-button');
+    expect(buttonTypeFilter[0]).toBeInTheDocument();
+
+    const psychic = screen.getByRole('button', { name: /psychic/i });
+    userEvent.click(psychic);
+    const alakazamPokemon = screen.getByText('Alakazam');
+    // getAllBy retorna um array
+    expect(alakazamPokemon).toBeInTheDocument();
+  });
+
+  test('verifica se a Pokédex contém um botão (All) para resetar o filtro', () => {
+    renderWithRouter(<App />);
+    const allBtn = screen.getByRole('button', {
+      name: /all/i,
+    });
+    userEvent.click(allBtn);
+    expect(allBtn).toBeInTheDocument();
+  });
+});
